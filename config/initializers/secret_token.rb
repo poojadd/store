f62dc673c564d99194aff8b575bf243835a5dd6b
Pooja <pooja@pooja-Lenovo-G550.(none)>
2014-02-04 13:01:18 +0530

    store app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = 'e1b5a9a08c5cd72f8174a2de4e2464b12663e0b06541b54859f8292bce3725aa8e8caff96941b172c16031b072a9b11db3cf64c8d81d62522bb8914a176b8d6e'
